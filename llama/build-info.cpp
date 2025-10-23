c0de7ad69e6a1603cc243d30c048d0773afd0bca
Daniel Hiltgen <daniel@ollama.com>
2025-10-23 15:06:02 -0700

    ggml update to b6829

--- llama/build-info.cpp
+++ llama/build-info.cpp
@@ -1,4 +1,4 @@
 int LLAMA_BUILD_NUMBER = 0;
-char const *LLAMA_COMMIT = "7049736b2dd9011bf819e298b844ebbc4b5afdc9";
+char const *LLAMA_COMMIT = "0bf47a1dbba4d36f2aff4e8c34b06210ba34e688";
 char const *LLAMA_COMPILER = "";
 char const *LLAMA_BUILD_TARGET = "";
