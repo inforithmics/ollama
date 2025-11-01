e12139035f06d80517e20543574c99b842537ef9
Inforithmics <thomas.stocker@gmail.com>
2025-11-01 22:44:08 +0100

    update to 0de0a01576772032008a689afc4d7c80685074c4

--- llama/build-info.cpp
+++ llama/build-info.cpp
@@ -1,4 +1,4 @@
 int LLAMA_BUILD_NUMBER = 0;
-char const *LLAMA_COMMIT = "d2d931f173b8a736b08999436e9259aafddec718";
+char const *LLAMA_COMMIT = "0de0a01576772032008a689afc4d7c80685074c4";
 char const *LLAMA_COMPILER = "";
 char const *LLAMA_BUILD_TARGET = "";
