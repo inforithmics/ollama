ad33e5d86e47b38980049c6fffa32557f5cf776d
Inforithmics <thomas.stocker@gmail.com>
2025-11-26 20:53:03 +0100

    use shared memory

--- ml/backend/ggml/ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ml/backend/ggml/ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -13199,7 +13199,7 @@ void ggml_backend_vk_get_device_memory(ggml_backend_vk_device_context *ctx, size
     // Check VRAM reporting for Windows IGPU/DGPU using DXGI + PDH (vendor agnostic)
     if (ggml_dxgi_pdh_init() == 0) {
         GGML_LOG_DEBUG("DXGI + PDH Initialized. Getting GPU free memory info\n");
-        int status = ggml_dxgi_pdh_get_device_memory(ctx->luid.c_str(), free, total, ctx->is_integrated_gpu);
+        int status = ggml_dxgi_pdh_get_device_memory(ctx->luid.c_str(), free, total, 1);
         if (status == 0) {
             GGML_LOG_DEBUG("%s utilizing DXGI + PDH memory reporting free: %zu total: %zu\n", __func__, *free, *total);
             ggml_dxgi_pdh_release();
