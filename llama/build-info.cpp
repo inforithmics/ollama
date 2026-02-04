41bec51680f6431063ea597e67d9718934ba1092
inforithmics <thomas.stocker@gmail.com>
2026-02-04 21:41:35 +0100

    Update to b7865

--- llama/build-info.cpp
+++ llama/build-info.cpp
@@ -1,4 +1,4 @@
 int LLAMA_BUILD_NUMBER = 0;
-char const *LLAMA_COMMIT = "a5bb8ba4c50257437630c136210396810741bbf7";
+char const *LLAMA_COMMIT = "f6b533d898ce84bae8d9fa8dfc6697ac087800bf";
 char const *LLAMA_COMPILER = "";
 char const *LLAMA_BUILD_TARGET = "";
